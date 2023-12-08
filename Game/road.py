fa6a930edd968f9aaeba98b510d754c57bbf50f6
Justice Plunkett <plunkettjustice@gmail.com>
2023-12-08 13:17:02 -0800

    Threaded zombie spawning

--- Game/road.py
+++ Game/road.py
@@ -10,6 +10,7 @@ from zombie import Zombie
 from car import Car
 import random
 import pygame
+import threading
 
 class RoadSide(GameObject):
     def __init__(self, main, transform : Transform, zOrder = 50, path = 'roads/straight/side'):
@@ -61,11 +62,13 @@ class Road(GameObject):
         self.children.append(RoadSide(self.main, Transform(Vec2(self.transform.pos.x+55, self.transform.pos.y), 180, Vec2(12, 144))))        
         RoadEnd(self.main, transform=Transform(self.transform.pos + Vec2(0, 72), scale=Vec2(85,85)))
         RoadDestroy(self.main, transform=Transform(self.transform.pos + Vec2(0, 720), scale=Vec2(85,85)),road=self)
-
         for i in range(random.randint(0,5)):
-            pos = Vec2(random.randint(-42,42), random.randint(-72,72)) + self.transform.pos
-            self.children.append(Zombie(self.main,Transform(pos,random.randint(-180,180),Vec2(3,3))))
+            x = threading.Thread(target=self.spawn_zombie)
+            x.start()
         print(f'Road spawned at: {self.transform.pos.xy}')
     def update(self):
         #self.transform.rot += 0.5
         pass
+    def spawn_zombie(self):
+        pos = Vec2(random.randint(-42,42), random.randint(-72,72)) + self.transform.pos
+        self.children.append(Zombie(self.main,Transform(pos,random.randint(-180,180),Vec2(3,3))))
