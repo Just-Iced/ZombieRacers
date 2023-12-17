2bb7854588380d84628a817f0773cac98f143bdc
RandomAccessGames <randomaccessgamesunidetified@gmail.com>
2023-12-17 00:09:36 -0800

    FUCK

--- Game/gamePlugin.py
+++ Game/gamePlugin.py
@@ -3,14 +3,18 @@ sys.path.append(os.getcwd())
 
 from Engine.main import main
 from Engine.transform import Transform
-from pygame.math import Vector2
+from pygame.math import Vector2 as Vec2
 from car import Car
 from grass import Grass
 from road import Road
-game = main()
+from zombie import Zombie
+from Engine.ParticleSystem.system import System
+from Engine.window import Window
+
+game = main(Window((1280, 720)))
 
 #put game logic here:
-car = Car(game, Transform(Vector2(90,90), 0, Vector2(16,16)))
-grass = Road(game, Transform(Vector2(90,45), 0, Vector2(16,16)))
+car = Car(game, Transform(Vec2(90,90), 0, Vec2(16,16)))
+road = Road(game, Transform(Vec2(90,144), 0, Vec2(85,16)))
 
 game.run()
