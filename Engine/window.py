c3ad0c4aeae9b26a6cb821d7f3850591e6281515
RandomAccessGames <randomaccessgamesunidetified@gmail.com>
2023-12-17 00:10:06 -0800

    ghbjm,

--- Engine/window.py
+++ Engine/window.py
@@ -1,12 +1,12 @@
 import pygame
 
 class Window:
-    def __init__(self):
+    def __init__(self, dimensions):
         
         pygame.init()
         
-        self.flags = (pygame.DOUBLEBUF)
+        self.flags = (pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.HWACCEL)
 
-        self.window = pygame.display.set_mode((1280, 720), self.flags, 16)
+        self.window = pygame.display.set_mode(dimensions, self.flags, vsync=1)
 
         pygame.display.set_caption("Zombie Racer")
