4e51017027bebc790fb1480fa52049ef449f280e
RandomAccessGames <randomaccessgamesunidetified@gmail.com>
2024-02-02 09:03:56 -0800

    Road Classes

---
+++ Game/roadGenerator.py
@@ -0,0 +1,24 @@
+import sys, os
+sys.path.append(os.getcwd())
+
+from Engine.gameObject import GameObject
+from Engine.transform import Transform
+from Engine.physicsObject import ColliderState
+from pygame.math import Vector2 as Vec2
+import pygame
+
+class RoadGenerator(GameObject):
+    def __init__(self, main, transform : Transform, zOrder = 10):
+        super().__init__(main, transform, zOrder)
+        #-CONSTRUCTOR-
+        
+        #Physics Parameters
+            #self.physics.scale = 0
+            #self.physics.simulate = True
+            #self.physics.minVel = Vec2(0, 0)
+            #self.physics.colliderState = ColliderState.Block
+        
+        
+    def update(self):
+        #put your object logic here
+        pass
---
+++ Game/roadPiece.py
@@ -0,0 +1,35 @@
+import sys, os
+from telnetlib import GA
+sys.path.append(os.getcwd())
+
+from Engine.gameObject import GameObject
+from Engine.spriteStack import SpriteStack
+from Engine.transform import Transform
+from Engine.physicsObject import ColliderState
+from pygame.math import Vector2 as Vec2
+from zombie import Zombie
+from car import Car
+from Engine.spawnMethod import SpawnMethod
+import random
+
+class Road(SpriteStack):
+    def __init__(self, main, transform : Transform, zOrder = 0):
+        super().__init__(main, transform, zOrder)
+        #-CONSTRUCTOR-
+        self.saveable = True
+        #Physics Parameters
+        self.physics.colliderState = ColliderState.Overlap
+        self.compatiblePieces = []
+        self.spawnPos = Vec2(0,0)
+    
+    def start(self):
+        if self.spawnMethod == SpawnMethod.Spawned:
+            for i in range(random.randint(0,5)):
+                self.spawn_zombie()
+        
+    def update(self):
+        #self.transform.rot += 0.5
+        pass
+    def spawn_zombie(self):
+        pos = Vec2(random.randint(-42,42), random.randint(-72,72)) + self.transform.pos
+        self.main.Instantiate(Zombie(self.main,Transform(pos,random.randint(-180,180),Vec2(3,3))))
