fea601797342664f54d1689f1d930e0a554935c0
DeuceyMan789 <plunkettjustice@gmail.com>
2024-02-09 10:52:33 -0800

    Fixed shop

--- Game/shop.py
+++ Game/shop.py
@@ -1,27 +1,26 @@
 import sys, os
 sys.path.append(os.getcwd())
 
-from Engine.gameObject import GameObject
+from Engine.spriteStack import SpriteStack
 from Engine.transform import Transform
 from Engine.physicsObject import ColliderState
 from pygame.math import Vector2 as Vec2
-from Engine.shadow import Shadow
+from Engine.ParticleSystem.system import System
+from Engine.gameObject import GameObject
 import pygame
+import time
 
-class Shop(GameObject):
+class Shop(SpriteStack):
     def __init__(self, main, transform : Transform, zOrder = 10):
         super().__init__(main, transform, zOrder)
         #-CONSTRUCTOR-
-        self.saveable = True        
-        self.shadow = Shadow(self)
-
+        self.saveable = True
         #Physics Parameters
         self.physics.scale = 0
         self.physics.simulate = True
         self.physics.minVel = Vec2(0, 0)
         self.physics.colliderState = ColliderState.Block
-        
-        
     def update(self):
         #put your object logic here
         pass
+    
